43227a83e6a73ea65cc1703c2b1dd0d9f02277dd
KevinColoma <kevincoloma@example.com>
2025-12-09 11:30:35 -0500

    Añadiendo codigo ejemplo

--- ArchivoEjemplo.cpp
+++ ArchivoEjemplo.cpp
@@ -2,7 +2,8 @@
 #include <string>
 #include <vector>
 
-
+// SEGURIDAD: Usar std::string maneja la memoria automáticamente.
+// No hay posibilidad de Buffer Overflow aquí.
 void saludar_usuario(const std::string& nombre) {
     if (nombre.length() > 50) {
         std::cout << "Nombre demasiado largo." << std::endl;
